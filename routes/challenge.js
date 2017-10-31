079780aba70b885ab0124eeabf715b55881c1078
codingkiwi <justin.reti@gmail.com>
2017-10-31 17:23:35 +1300

    add challenge form insterts to DB

--- routes/challenge.js
+++ routes/challenge.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var csrf = require('csurf');
 var passport = require('passport');
+var Challenge = require('../models/challenge');
 
 var csrfProtection = csrf();
 router.use(csrfProtection);
@@ -18,6 +19,36 @@ router.get('/', function(req, res, next){
 router.get('/create-challenge', function(req, res, next){
     var messages = req.flash('error');
     res.render('challenges/create-challenge', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/create-challenge', function(req, res, next){
+    var newChallenge = new Challenge({
+        name : req.body.name,
+        description: req.body.description,
+        startDate : req.body.startDate,
+        endDate : req.body.endDate,
+        creationDate : req.body.startDate,
+        goal : req.body.goal,
+        participants : [
+            {
+                participantID: req.user.id,
+                participantName: req.user.name,
+                participantRole: "admin"
+            }
+        ],
+        categories :  [req.body.categories]      
+    });
+    newChallenge.save(function(err, result) {
+        if (err) {
+            console.log(err);
+            var messages = req.flash('error');
+            res.render('/create-challenge');
+        }
+        else {
+            res.redirect('/user/dashboard');
+            console.log("redirected to dashboard")
+        }
+    });
 });
 
 module.exports = router;
